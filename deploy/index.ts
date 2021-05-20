522cd66305f891065015d4c7d97b63e763954968
smallkirby <ssmallkirby@gmail.com>
2021-05-20 15:17:35 +0900

    a

--- deploy/index.ts
+++ deploy/index.ts
@@ -24,11 +24,8 @@ app.post('/hooks/github', async (req, res) => {
     return 'pong';
   }
   if (event === 'push') {
-    console.log(req.headers);
-    console.log(get(req.headers, ["X-Hub-Signature-256"]));
-    console.log("***");
-    console.log(get(req.headers, "X-Hub-Signature-256"));
-    const xsig = Buffer.from(get(req.headers, ["X-Hub-Signature-256"]) as string, 'utf8');
+    console.log(get(req.headers, ["x-hub-signature-256"]));
+    const xsig = Buffer.from(get(req.headers, ["x-hub-Signature-256"]) as string, 'utf8');
     const hmac = crypto.createHmac('sha256', process.env.GITHUB_SECRET as string);
     const digest = Buffer.from('sha256=' + hmac.update(req.body).digest('hex'), 'utf8');
     if (!crypto.timingSafeEqual(digest, xsig)) {
