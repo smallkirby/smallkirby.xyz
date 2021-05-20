d08f6efcc5a7bf9c1ffb1d982782e50eb91dd5f1
smallkirby <ssmallkirby@gmail.com>
2021-05-20 15:46:28 +0900

    auto-deploy

--- deploy/index.ts
+++ deploy/index.ts
@@ -3,7 +3,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { get } from 'lodash';
 import crypto from 'crypto';
-import bodyParser from 'body-parser';
+import { spawn } from 'child_process';
+import concat from 'concat-stream';
+import { PassThrough } from 'stream';
 
 dotenv.config();
 export const app = express();
@@ -24,6 +26,7 @@ app.post('/hooks/github', async (req, res) => {
     return 'pong';
   }
   if (event === 'push') {
+    // validation
     const xsig = Buffer.from(get(req.headers, ["x-hub-signature-256"]) as string, 'utf8');
     const hmac = crypto.createHmac('sha256', process.env.GITHUB_SECRET as string);
     const digest = Buffer.from('sha256=' + hmac.update(req.body).digest('hex'), 'utf8');
@@ -35,7 +38,7 @@ app.post('/hooks/github', async (req, res) => {
       return;
     }
 
-    const preq = JSON.parse(req.toString());
+    const preq = JSON.parse(req.body.toString('utf-8'));
     console.log(preq);
     if (get(preq, ['repository', 'id']) !== 368909618) {
       res.status(404);
@@ -47,9 +50,40 @@ app.post('/hooks/github', async (req, res) => {
       res.send('invalid refs.');
       return;
     }
+
+    // send status
+    console.log('received valid hook');
+    res.status(200);
+    res.send('OK');
+
+    // deploy
+    for (const [command, ...args] of commands) {
+      const proc = spawn(command, args, { cwd: process.cwd() });
+      const muxed = new PassThrough();
+
+      proc.stdout.on('data', (chunk) => muxed.write(chunk));
+      proc.stdout.on('data', (chunk) => muxed.write(chunk));
+
+      Promise.all([
+        new Promise<void>((resolve) => proc.stdout.on('end', () => resolve())),
+        new Promise<void>((resolve) => proc.stdout.on('end', () => resolve())),
+      ]).then(() => {
+        muxed.end();
+      });
+
+      const output = await new Promise<Buffer>((resolve) => {
+        muxed.pipe(concat({ encoding: 'buffer' }, (data: Buffer) => {
+          resolve(data);
+        }));
+      })
+
+      console.log(output);
+    }
+
+    return;
   }
   res.status(501);
-  return 'not found';
+  res.send('not found');
 });
 
 app.listen(3001);
