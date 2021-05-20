3f839f26ab59fdcf1e0063a41500ff752d253550
smallkirby <ssmallkirby@gmail.com>
2021-05-20 15:07:01 +0900

    add secret verification

--- deploy/index.ts
+++ deploy/index.ts
@@ -1,8 +1,14 @@
-import fastify from 'fastify';
+//import fastify from 'fastify';
+import express from 'express';
 import dotenv from 'dotenv';
+import { get } from 'lodash';
+import crypto from 'crypto';
+import bodyParser from 'body-parser';
 
 dotenv.config();
-export const app = fastify();
+export const app = express();
+
+app.use('/hooks/github', express.raw({ type: 'application/json' }));
 
 const commands = [
   ['git', 'fetch', '--all'],
@@ -18,15 +24,27 @@ app.post('/hooks/github', async (req, res) => {
     return 'pong';
   }
   if (event === 'push') {
-    console.log(req)
-    res.code(202);
-    return 'not yet';
+    const xsig = Buffer.from(get(req.headers, ["X-Hub-Signature-256"]) as string, 'utf8');
+    const hmac = crypto.createHmac('sha256', process.env.GITHUB_SECRET as string);
+    const digest = Buffer.from('sha256=' + hmac.update(req.body).digest('hex'), 'utf8');
+    if (!crypto.timingSafeEqual(digest, xsig)) {
+      console.log(digest);
+      console.log(xsig);
+      res.status(404);
+      res.send('secret verification error.');
+    }
+    if(get)
+    if (get(req.body, ['repository', 'id']) !== 386909618) {
+      res.status(404);
+      res.send('repository ID is invalid.');
+    }
+    if (get(req.body, ['ref']) !== 'refs/heads/master') {
+      res.status(404);
+      res.send('invalid refs.');
+    }
   }
-  res.code(501);
+  res.status(501);
   return 'not found';
 });
 
-app.listen(3001, (err, addr) => {
-  if (err) throw err;
-  app.log.info(`server listening on ${addr}`);
-})
+app.listen(3001);
