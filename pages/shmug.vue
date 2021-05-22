fb19e13eb77ff37ca28e21409488fee5b00833d0
smallkirby <ssmallkirby@gmail.com>
2021-05-22 23:20:18 +0900

    shmug

---
+++ pages/shmug.vue
@@ -0,0 +1,40 @@
+<template>
+  <layout-wrapper>
+    <layout-header title="about" />
+    <div>
+      <div class="main-window">
+        <p>c|_|</p>
+      </div>
+    </div>
+  </layout-wrapper>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'shmug',
+  data() {
+    return {
+      titleMsg: "$ cat ./about.txt",
+      flagTitleMsg: false,
+    }
+  },
+  methods: {
+    finTitleMsg(): void{
+      this.flagTitleMsg = true;
+    }
+  },
+})
+</script>
+
+<style>
+div.center-normal {
+  text-align: center;
+}
+
+img#kirby-pict {
+  margin: 0 auto;
+}
+
+</style>
