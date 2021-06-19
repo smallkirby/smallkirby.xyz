07b87564457f1dcf3ea4d2dd2fd56ab2c71635aa
smallkirby <ssmallkirby@gmail.com>
2021-06-20 08:58:11 +0900

    add nirugiri counter

---
+++ components/LayoutLineChart.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { Line, mixins } from 'vue-chartjs';
+const { reactiveProp } = mixins;
+
+export default {
+  name: 'LayoutLineChar',
+  mixins: [Line, reactiveProp],
+  props: ['options'],
+  mounted () {
+    // @ts-ignore
+    this.renderChart(this.data, this.options);
+  },
+};
+</script>
+
+<style>
+div.center-normal {
+  text-align: center;
+}
+
+</style>
--- pages/nirugiri.vue
+++ pages/nirugiri.vue
@@ -4,7 +4,13 @@
     <div>
       <div class="main-window">
         <div id="nirugiri">
-          <p>c|_|</p>
+          <layout-line-chart 
+            :chart-data="datacollection" 
+            :height="chart_height"
+            :width="chart_width"
+            :options="options"
+            :styles="{'display':'flex', 'justify-content':'center'}"
+          />
         </div>
       </div>
     </div>
@@ -12,7 +18,9 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+import axios from 'axios';
+import { range } from 'lodash';
 
 export default Vue.extend({
   name: 'Nirugiri',
@@ -20,6 +28,10 @@ export default Vue.extend({
     return {
       titleMsg: '$ cat ./about.txt',
       flagTitleMsg: false,
+      datacollection: null,
+      options: null,
+      chart_height: 200,
+      chart_width: 350,
     }
   },
   methods: {
@@ -27,12 +39,75 @@ export default Vue.extend({
       this.flagTitleMsg = true
     },
   },
+  async created() {
+    const { data } = await axios.get('/nirugiri.json');
+    const counts: number[] = data.counts;
+    const labels = range(0, counts.length).map(n => n.toString());
+
+    this.datacollection = {
+      labels: labels,
+      datasets: [{
+        label: "Total",
+        data: counts,
+        borderColor: "#ebdbb2",
+        pointBackgroundColor: "ebdbb2",
+      }],
+    };
+
+    this.options = {
+      responsive: true,
+      title: {
+        display: true,
+        position: 'top',
+        fontSize: 15,
+        fontColor: "#ebdbb2",
+        text: 'Nirugiri Counter',
+      },
+      legend: {
+        display: true,
+      },
+      scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: '???',
+          },
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'counts',
+            beginAtZero: false,
+          },
+        }],
+      },
+    };
+  }
 })
 </script>
 
 <style>
 div.center-normal {
   text-align: center;
+}
+
+img#kirby-pict {
+  margin: 0 auto;
+}
+
+div#nirugiri {
+  width: 80vw;
+  height: 80vh;
+  text-align: center;
+  vertical-align: middle;
+  margin: 0 auto;
+}
+
+canvas {
+  text-align: center;
+  display: inline-block;
+  vertical-align: middle;
+  margin: 0 auto;
 }
 
 </style>
