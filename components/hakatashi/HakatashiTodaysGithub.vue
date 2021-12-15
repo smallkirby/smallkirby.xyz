153760af4d1417f9751f4066acdc53d87692c657
smallkirby <ssmallkirby@gmail.com>
2021-12-15 23:11:05 +0900

    add template of hakatashi

---
+++ components/hakatashi/HakatashiTodaysGithub.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout-wrapper>
+    <div class="mt-8 w-9/12 mx-auto flex border py-4 px-4 border-skwhite-dark">
+      <div class="text-4xl text-skblue">
+        <p>Today's Activity</p>
+      </div>
+    </div>
+  </layout-wrapper>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HakatashiTodaysGithub',
+});
+</script>
+
+<style></style>
