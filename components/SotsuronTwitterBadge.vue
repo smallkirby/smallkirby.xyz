6fb7bbae37b0c55e56fe1d1c629427f19baf7549
smallkirby <ssmallkirby@gmail.com>
2022-02-10 15:45:48 +0900

    ♻️ show done pages on sotsuron tweet badge

--- components/SotsuronTwitterBadge.vue
+++ components/SotsuronTwitterBadge.vue
@@ -9,9 +9,13 @@
             <p class="font-bold mr-2">
               largekirby
             </p>
-            <a class="text-skwhite-dark mr-2" href="https://twitter.com/smallkirby_">
+            <a
+              class="text-skwhite-dark mr-2"
+              href="https://twitter.com/smallkirby_"
+            >
               @smallkirby_
             </a>
+            <p>({{ tweet.done_pages }} P)</p>
           </div>
           <div class="text-skwhite-dark">
             {{ timestampPretty }}
@@ -25,14 +29,35 @@
 
         <!-- icons row -->
         <div class="mt-1 flex w-full md:w-1/2 justify-between mx-auto">
-          <a href="http://www.catb.org/~esr/faqs/hacker-howto.html" target="_blank">
-            <img :src="current_fukidashi_image" class="w-6 fill-white" @mouseover="flip_fukidashi_mouseon" @mouseleave="flip_fukidashi_mouseon">
+          <a
+            href="http://www.catb.org/~esr/faqs/hacker-howto.html"
+            target="_blank"
+          >
+            <img
+              :src="current_fukidashi_image"
+              class="w-6 fill-white"
+              @mouseover="flip_fukidashi_mouseon"
+              @mouseleave="flip_fukidashi_mouseon"
+            >
           </a>
-          <a href="https://twitter.com/intent/tweet?text=DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21" target="_blank">
-            <img :src="current_retweet_image" class="w-6 fill-white" @mouseover="flip_retweet_mouseon" @mouseleave="flip_retweet_mouseon">
+          <a
+            href="https://twitter.com/intent/tweet?text=DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21DON%27T%20RETWEET%20PRIVATE%20ACOUNT%21%21%21"
+            target="_blank"
+          >
+            <img
+              :src="current_retweet_image"
+              class="w-6 fill-white"
+              @mouseover="flip_retweet_mouseon"
+              @mouseleave="flip_retweet_mouseon"
+            >
           </a>
           <a href="https://kaguya.love" target="_blank">
-            <img :src="current_fav_image" class="w-6 fill-white" @mouseover="flip_fav_mouseon" @mouseleave="flip_fav_mouseon">
+            <img
+              :src="current_fav_image"
+              class="w-6 fill-white"
+              @mouseover="flip_fav_mouseon"
+              @mouseleave="flip_fav_mouseon"
+            >
           </a>
         </div>
       </div>
@@ -70,17 +95,23 @@ export default Vue.extend({
 
     // eslint-disable-next-line object-shorthand
     current_fav_image: function () {
-      return (this.$data as any).fav_image_mouseon ? require('~/static/img/sotsuron/heart-br.png') : require('~/static/img/sotsuron/heart.png');
+      return (this.$data as any).fav_image_mouseon
+        ? require('~/static/img/sotsuron/heart-br.png')
+        : require('~/static/img/sotsuron/heart.png');
     },
 
     // eslint-disable-next-line object-shorthand
     current_fukidashi_image: function () {
-      return (this.$data as any).fukidashi_image_mouseon ? require('~/static/img/sotsuron/fukidashi-zzz.png') : require('~/static/img/sotsuron/fukidashi.png');
+      return (this.$data as any).fukidashi_image_mouseon
+        ? require('~/static/img/sotsuron/fukidashi-zzz.png')
+        : require('~/static/img/sotsuron/fukidashi.png');
     },
 
     // eslint-disable-next-line object-shorthand
     current_retweet_image: function () {
-      return (this.$data as any).retweet_image_mouseon ? require('~/static/img/sotsuron/retweet-white.png') : require('~/static/img/sotsuron/retweet.png');
+      return (this.$data as any).retweet_image_mouseon
+        ? require('~/static/img/sotsuron/retweet-white.png')
+        : require('~/static/img/sotsuron/retweet.png');
     },
   },
 
@@ -89,10 +120,12 @@ export default Vue.extend({
       (this as any).fav_image_mouseon = !(this as any).fav_image_mouseon;
     },
     flip_fukidashi_mouseon () {
-      (this as any).fukidashi_image_mouseon = !(this as any).fukidashi_image_mouseon;
+      (this as any).fukidashi_image_mouseon = !(this as any)
+        .fukidashi_image_mouseon;
     },
     flip_retweet_mouseon () {
-      (this as any).retweet_image_mouseon = !(this as any).retweet_image_mouseon;
+      (this as any).retweet_image_mouseon = !(this as any)
+        .retweet_image_mouseon;
     },
   },
 });
